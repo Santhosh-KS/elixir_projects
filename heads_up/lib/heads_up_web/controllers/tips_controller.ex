9b8b59aeed454b52e109681f003a84a463bed984
Santhosh K S <santhoshachar08@gmail.com>
2025-01-05 15:50:14 +0530

    06 Router parameters

--- heads_up/lib/heads_up_web/controllers/tips_controller.ex
+++ heads_up/lib/heads_up_web/controllers/tips_controller.ex
@@ -2,8 +2,13 @@ defmodule HeadsUpWeb.TipsController do
   use HeadsUpWeb, :controller
 
   def tips(conn, _params) do
-    emojis = ~w(💚 💜 💙) |> Enum.random() |> String.duplicate(5)
+    emojis = ~w(💚 💜 💙) |> Enum.random() |> String.duplicate(1..5 |> Enum.random())
     rules = HeadsUp.Tips.list_tips()
-    render(conn, :tips, emojis: emojis, mytips: rules)
+    render(conn, :index, emojis: emojis, mytips: rules)
+  end
+
+  def show(conn, %{"id" => id}) do
+    tip = HeadsUp.Tips.get_tip(id)
+    render(conn, :show, tip: tip)
   end
 end
