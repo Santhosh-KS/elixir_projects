a485b24d13afd9007f06d095bc1d6d206567a9de
Santhosh K S <santhoshachar08@gmail.com>
2025-03-01 16:20:01 +0530

    Do Fun things with Big Loud worker bees

---
+++ mastery/test/question_test.exs
@@ -0,0 +1,22 @@
+defmodule QuestionTest do
+  use ExUnit.Case
+  use QuizBuilders
+
+  test "building chooses substitutions" do
+    question = build_question(generators: addition_generators([1], [2]))
+    assert question.substitutions == [left: 1, right: 2]
+  end
+
+  test "function generators are called" do
+    generators = addition_generators(fn -> 42 end, [0])
+    substitutions = build_question(generators: generators).substitutions
+
+    assert Keyword.fetch!(substitutions, :left) == generators.left.()
+  end
+
+  test "biulding creates asked question text" do
+    question = build_question(generators: addition_generators([1], [2]))
+
+    assert question.asked == "1 + 2"
+  end
+end
