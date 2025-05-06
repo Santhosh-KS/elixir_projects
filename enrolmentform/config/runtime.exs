8dd5583eb5e0c61e547aef0ae0f8ca9cad81d857
Santhosh K S <santman@santman-2.local>
2025-05-06 14:49:10 +0530

    rel experiments

--- enrolmentform/config/runtime.exs
+++ enrolmentform/config/runtime.exs
@@ -21,22 +21,26 @@ if System.get_env("PHX_SERVER") do
 end
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+  database_url = "ecto://postgres:Pa55word@localhost:5431/enrolmentform_prod_t1"
+  # database_url =
+  #   System.get_env("DATABASE_URL") ||
+  #     raise """
+  #     environment variable DATABASE_URL is missing.
+  #     For example: ecto://USER:PASS@HOST/DATABASE
+  #     """
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :enrolmentform, Enrolmentform.Repo,
-    # ssl: true,
+    ssl: true,
+    ssl_opts: [verify: :verify_none],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     # For machines with several cores, consider starting multiple pools of `pool_size`
     # pool_count: 4,
-    socket_options: maybe_ipv6
+    socket_options: maybe_ipv6,
+    queue_target: 5000,
+    queue_interval: 5000
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
---
+++ enrolmentform/rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\enrolmentform" eval Enrolmentform.Release.migrate
